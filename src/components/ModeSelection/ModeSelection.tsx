b7e7e918113d0f68800516f3c68ca0c9bf13b6ac
Sebastian Zapata <sebastian.zapata@bri56226l.hq.peninsula-uk.local>
2025-10-06 15:38:43 +0100

    Update mode selection buttons

--- src/components/ModeSelection/ModeSelection.tsx
+++ src/components/ModeSelection/ModeSelection.tsx
@@ -6,22 +6,22 @@ const ModeSelection = () => {
   const { setSelectedMode } = useSpotify();
 
   return (
-    <div className="flex flex-col sm:flex-row gap-12">
+    <div className="flex flex-col sm:flex-row gap-12 max-h-screen sm:max-h-none">
       <button
         onClick={() => setSelectedMode("History")}
-        className="flex flex-col gap-c8 bg-transparent border-neutral-500 border-8 rounded-3xl p-8"
+        className="flex flex-col gap-c8 bg-transparent border-neutral-500 border-8 rounded-3xl p-8 max-h-[40vh] sm:max-h-none"
       >
         <HistoryIcon lightTheme={false} />
 
-        <h2>Listening history</h2>
+        <h2 className="text-lg sm:text-2xl">Listening history</h2>
       </button>
       <button
         onClick={() => setSelectedMode("Followed")}
-        className="flex flex-col gap-c8 bg-transparent border-neutral-500 border-8 rounded-3xl p-8"
+        className="flex flex-col gap-c8 bg-transparent border-neutral-500 border-8 rounded-3xl p-8 max-h-[40vh] sm:max-h-none"
       >
         <FollowedArtistsIcon lightTheme={false} />
 
-        <h2>Followed artists</h2>
+        <h2 className="text-lg sm:text-2xl">Followed artists</h2>
       </button>
     </div>
   );
